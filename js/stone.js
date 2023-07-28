a460bb0d20725636c38c4683b073c49bf891f7b9
verygoodtuna <kjy990504@gmail.com>
2023-07-28 18:18:16 +0900

    blue

--- js/stone.js
+++ js/stone.js
@@ -1,5 +1,12 @@
 $(function () {
 
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        sct > 0
+            ? $('.header').addClass('on')
+            : $('.header').removeClass('on');
+    });
+
     $('.p_slide').slick({
         arrows: false,
     })
