bd8edd6ddf996791446d50bf8bb4625a416a7df3
verygoodtuna <kjy990504@gmail.com>
2023-07-28 17:46:19 +0900

    stoong

--- js/stone.js
+++ js/stone.js
@@ -1,9 +1,4 @@
 $(function () {
-    $('.main_con').fullpage({
-        anchors: ['main_studio', 'main_about', 'main_goods', 'main_art', 'main_together'],
-        navigation: false,
-        css3: false,
-    })
 
     $('.p_slide').slick({
         arrows: false,
